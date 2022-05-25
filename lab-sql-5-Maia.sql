5e970ad4ba7e56848a8363c4ae62d56b77b2f10e
Maia Carolina Pozzolo <maia.pozzolo@auctane.com>
2022-05-25 19:35:30 +0200

    update

---
+++ lab-sql-5-Maia.sql
@@ -0,0 +1,11 @@
+alter table staff
+drop column picture;
+
+select * from sakila.staff;
+
+insert into staff
+values (75,'Tammy','Sanders',79,'tammy.sanders@sakilacustomer.org',2,1,'tammy',0,'2006-02-15 04:57:20');
+
+
+
+
